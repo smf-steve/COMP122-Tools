52d5c869aea330d09b0e004411b5d0023cb4175b
Steve Fitzgerald <steven.fitzgerald@my.csun.edu>
2022-06-03 16:49:45 -0700

    reorg

---
+++ json-definition/README.md
@@ -0,0 +1,103 @@
+# JSON File Format
+Our MIPS assembler produces a .json file that represents a valid and assembled MIPS program.  This file is consumed by our MIPS simulator, as well as other tools.  This directory contains the specification for this file format.
+
+This specification of this file format is defined via the combination of a BNF 
+
+Program -> 
+```json
+Program -> { "program": null, // A general 
+    "timestamp": tag ,
+    "entry": "main",
+    "source_files": [], // an array of text representing the original source program
+    "labels": {   // the symbol table of all labels
+          "defines": [], "macros": [], "data" : [], "text" : [] 
+          }, // See symbol_table.md for more information
+
+
+    "overlays": [ // an array of segments defined by the user's program, with each segment providing an array of declarations or instructions
+                  
+    ],
+ 
+   // DATA: 
+   //    - source: defines the declarations as defined textual, with the given structure
+   //    - memory: defines the raw values in memory, without any labels
+   //    - overlay: defines the associate labels, alignment, structure, etc.
+
+   // TEXT
+   //    - source: defines the instructions as defined textual
+   //    - memory: defines the encode instructions, without any any labels
+   //    - overlay: defines the associate labels, structure (associated structure), etc.
+   //               moreover the overlay does not contain non native instructions
+
+
+    "core": { 
+    }
+}
+
+
+
+
+=========
+
+
+```json
+{ "program": "<general description>",
+    "entry": "main",
+    "source_files": [], // an array of text representing the original source program
+    "labels": {   // the symbol table of all labels
+          "defines": [], "macros": [], "data" : [], "text" : [] 
+          }, // See symbol_table.md for more information
+
+
+    "overlays": [ // an array of segments defined by the user's program, with each segment providing an array of declarations or instructions
+                  { "segment": ".text",   //
+                    "alignment": "off",   //  { "on", "off" }     : Indicates if memory alignment was performed
+                    "wordsize": 4,        //  --> Is this necessary?
+                    "endianness": "big",  //  { "big", "little" } : Indicates the endianness used for byte ordering
+                    "address": null,      //  The starting address of this associated segment
+                    "size": null,         //  The number of bytes associated with the segment
+                    "comment": null,      //  The comment that appeared on the ".text" directive line
+                    "statements": []      //  The list of statements defined within the segment
+                  },
+    ],
+ 
+   // DATA: 
+   //    - source: defines the declarations as defined textual, with the given structure
+   //    - memory: defines the raw values in memory, without any labels
+   //    - overlay: defines the associate labels, alignment, structure, etc.
+
+   // TEXT
+   //    - source: defines the instructions as defined textual
+   //    - memory: defines the encode instructions, without any any labels
+   //    - overlay: defines the associate labels, structure (associated structure), etc.
+   //               moreover the overlay does not contain non native instructions
+
+
+    "core": { 
+        "memory": [   // an array of named segments within main memory
+            { "segment": ".data", "start": "0x01001000", "end": "0x01001000",
+              "values": [ 
+                {"address": "0xFFFF", "value": "0xFFFF" },
+                {"address": "0xFFFF", "value": "0xFFFF" },
+                {"address": "0xFFFF", "value": "0xFFFF" } 
+                // any memory address not enumerated implies that value is undetermined
+             ]
+            },
+            { "segment": ".text", "start": "0x01001000", "end": "0x01001000", 
+              "values": [
+                {"address": "0xFFFF", "value": "0xFFFF" },
+                {"address": "0xFFFF", "value": "0xFFFF" },
+                {"address": "0xFFFF", "value": "0xFFFF" },
+                {"address": "0xFFFF", "value": "0xFFFF" }
+                // any memory address not enumerated implies error
+             ]
+            }
+            // additional memory segments can be provided
+        ],
+        "CPU": [ ], // a register array with current values
+        "CPO": [ ], // a register array with current values
+        "FPU": [ ] // a register array with current values
+    }
+}
+
+
--- sample/instructions.asm
+++ sample/instructions.asm
@@ -1,3 +1,4 @@
+
 /* 
 Title:    instruction.asm
 Purpose:  to provide a sample set of MIPS declaration for testing
@@ -13,7 +14,7 @@ File is mainly defunct!
 
  .text
 
-1. main:   nop # Starting point of the program
+main:   nop # Starting point of the program
         nop
 
 sum:    li $t2, 2
@@ -33,7 +34,7 @@ done:   nop                             # We completed the for-loop
 
 
 
-/* - Corresponding Processed Source File
+// - Corresponding Processed Source File
 
 
 I don't think I have to retain comments in the processe source...
@@ -43,50 +44,49 @@ I don't think I have to retain comments in the processe source...
 //  program:  data
 //    { "line": 14, "label": "",   "op": ".byte",   "values": [ { value: 0, count: 1} ],          "comment": "# Comment String" },
 
-{ program: "instructions.asm", instructions: [
-    { segment: ".txt", address: 0x1000, [
+{ "program": "instructions.asm", "instructions": [
+    { "segment": ".txt", "address": "0x1000", "source": [
 
-//      {line: 13, "label: "main", "op": "nop", argc: 3, args: [ "", "", "" ] }, "comment": "# Comment String" },
+        {"line": 13, "label": "main", "op": "nop", "argc": 3, "args": [ "", "", "" ], "comment": "# Comment String" },
 
 
-        {line: 13, "label: "main", format: S, "op": "nop",   count: 1,  rd: "", rs: "", rt: "", imm: "", "comment": "# Comment String" },},
-        {line: 14, "label: "",     format: s, "op": "nop",   count: 1,  rd: "", rs: "", rt: "", imm: "" },
+        {"line": 13, "label" : "main", "format": "S", "op": "nop",   "bytes": 4, "rd": "",     "rs": "",      "rt": "", "imm": "", "comment": "# Comment String" },},
+        {"line": 14, "label" : "",     "format": "S", "op": "nop",   "bytes": 4, "rd": "",     "rs": "",      "rt": "", "imm": "" },
 
-        {line: 16, "label: "",     format: I, "op": "addiu", count: 1, rd: "$t2", rs: "$0", rt: "", imm: "2" },
-        {line: 17, "label: "",     format: S, "op": "",      count: 1, rd: "", rs: "", rt: "", imm: "" },
-        {line: 18, "label: "",     format: S, "op": "nop",   count: 1, rd: "", rs: "", rt: "", imm: "" },
+        {"line": 16, "label" : "",     "format": "I", "op": "addiu", "bytes": 4, "rd": "$t2",  "rs": "$0",    "rt": "", "imm": "2" },
+        {"line": 17, "label" : "",     "format": "S", "op": "",      "bytes": 4, "rd": "",     "rs": "",      "rt": "", "imm": "" },
+        {"line": 18, "label" : "",     "format": "S", "op": "nop",   "bytes": 4, "rd": "",     "rs": "",      "rt": "", "imm": "" },
 
-        {line: 19, "label: "",     format: R, op": "addu",   count: 1, rd: "$t2", rs: "$zero", rt: "2", imm: "" },
+        {"line": 19, "label" : "",     "format": "R", "op": "addu",   "bytes": 4, "rd": "$t2", "rs": "$zero", "rt": "2", "imm": "" },
 
-        {line: 20, "label: "",     format: B, "op": "",      count: 1, rd: "", rs: "", rt: "", imm: "" },
-        {line: 21, "label: "loop", format: S, "op": "",      count: 1, rd: "", rs: "", rt: "", imm: "" },
+        {"line": 20, "label" : "",     "format": "B", "op": "",      "bytes": 4, "rd": "",     "rs": "",      "rt": "", "imm": "" },
+        {"line": 21, "label" : "loop", "format": "S", "op": "",      "bytes": 4, "rd": "",     "rs": "",      "rt": "", "imm": "" },
  
-        {line: 22, "label: "top",  format: S, "op": "nop",   count: 1, rd: "", rs: "", rt: "", imm: "" },
+        {"line": 22, "label" : "top",  "format": "S", "op": "nop",   "bytes": 4, "rd": "",     "rs": "",      "rt": "", "imm": "" },
  
-        {line: 23, "label: "",     format: R, "op": "slt",   count: 1, rd: "$1", rs: "$t6", rt: "$t7", imm: "" },
-        {line: 23, "label: "",     format: I, "op": "beq",   count: 2, rd: "$1", rs: "$zero", rt: "", imm: "done" },
+        {"line": 23, "label" : "",     "format": "R", "op": "slt",   "bytes": 4, "rd": "$1",   "rs": "$t6",   "rt": "$t7", "imm": "" },
+        {"line": 23, "label" : "",     "format": "P", "op": "bge",   "bytes": 8, "rd": "$1",   "rs": "$zero", "rt": "", "imm": "done" },
 
 
-        {line: 24, "label: "",     format: I, "op": "sw",  count: 1, rd: "", rs: "", rt: "", imm: "" },
-        {line: 25, "label: "",     format: I, "op": "lw",  count: 1, rd: "", rs: "", rt: "", imm: "" },
-        {line: 26, "label: "",     format: P, "op": "nop", count: 1, rd: "", rs: "", rt: "", imm: "" },
-        {line: 27, "label: "",     format: R, "op": "addi",count: 1, rd: "$t6", rs: "$t6", rt: "", imm: "1" },
-        {line: 28, "label: "",     format: J, "op": "j",   count: 1, rd: "", rs: "", rt: "", imm: "" },
-        {line: 29, "label: "done", format: S, "op": "nop", count: 1, rd: "", rs: "", rt: "", imm: "" },
+        {"line": 24, "label" : "",     "format": "I", "op": "sw",    "bytes": 4, "rd": "", "rs": "", "rt": "", "imm": "" },
+        {"line": 25, "label" : "",     "format": "I", "op": "lw",    "bytes": 4, "rd": "", "rs": "", "rt": "", "imm": "" },
+        {"line": 26, "label" : "",     "format": "P", "op": "nop",   "bytes": 4, "rd": "", "rs": "", "rt": "", "imm": "" },
+        {"line": 27, "label" : "",     "format": "R", "op": "addi",  "bytes": 4, "rd": "", "rs": "", "rt": "", "imm": "1" },
+        {"line": 28, "label" : "",     "format": "J", "op": "j",     "bytes": 4, "rd": "", "rs": "", "rt": "", "imm": "" },
+        {"line": 29, "label" : "done", "format": "S", "op": "nop",   "bytes": 4, "rd": "", "rs": "", "rt": "", "imm": "" },
 ]
 }
 
 // The above structure should provide the "parsed" representation of instructions.
 //  This structure place the instruction table can be usued to generate the text overlay.
 // this structure should match the syntax.
-   -- three address
-   -- two address
-   -- one address, e.g., mtlo $s1
-   -- addressing modes, direction, immediate, indirect
+//    -- three address
+//    -- two address
+//    -- one address, e.g., mtlo $s1
+//    -- addressing modes, direction, immediate, indirect
 
-
-Should the memory overlay contain the Format of the the instructions
-Then the memory would contain the raw data..
+// Should the memory overlay contain the Format of the the instructions
+// Then the memory would contain the raw data..
 
 // data overlay
 // { "line": 0, "labels": "",   "op": "#slot",  "size": 3, "byte": 0, "address": TBD },
@@ -94,9 +94,10 @@ Then the memory would contain the raw data..
 
 //  The overlay should provide the view of all four words as an aggregate
 
-text_overlay ->  { "segment": ".text", "alignment": "on", "wordsize": 4, "endianness": "big", start": 0x0040 000, "size": N, "values": [
+text_overlay ->  
+{ "segment": ".text", "alignment": "on", "wordsize": 4, "endianness": "big", start": 0x0040 000, "size": N, "values": [
     { line: N, "labels": [ "main" ] format: I ,  opcode: "", rs:, rt:, imm: }
-    { line: N, "labels": [ "main" ] format: R ,  opcode: rs:, rt:, rd: sh: func:
+    { line: N, "labels": [ "main" ] format: R ,  opcode: "", rs:, rt:, rd: sh: func:
     { line: N, "labels": [ "main" ] format: J ,  opcode: 2,  "address": }
 
 
